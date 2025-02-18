ecdfa8324e3804e0a82098ede429c0c23177bfe6
elenakozlova28 <elenakozlova2828@gmail.com>
2025-02-18 21:29:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true);
   const pathname = parsedUrl.pathname;
-
-
   
   // Logger Function
   const logRequest = () => {
